0a2382d2b456c0a9c628d91ee65540c69c837405
WelComeRai <155359687+WelComeRai@users.noreply.github.com>
2025-03-23 15:37:40 +0500

    utils

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+
 // import functions
 import ConnectToDb from "./db/ConnectDB.js";
-ConnectToDb();
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("<h1>Your on Home page</h1>");
-});
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`App is running port ${process.env.PORT}`);
-});
+import { app } from "./app.js";
+ConnectToDb()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server is runing at port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Connection Failed !!!!!", error);
+  });
